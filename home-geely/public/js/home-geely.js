d630d461a8a8eec39f87d748dd3132c0cd2a8672
IHuan123 <42716044+IHuan123@users.noreply.github.com>
2018-11-03 18:07:30 +0800

    Add files via upload

--- home-geely/public/js/home-geely.js
+++ home-geely/public/js/home-geely.js
@@ -301,23 +301,33 @@
 (function(){
     var $ups=document.querySelectorAll(".footer-up");
     var footerCosle=document.querySelectorAll(".footer-cosle");
+    var $footer_option=$(".footer-option")
     var $html=document.documentElement;
-
-        var i=0;
-        for(var up of $ups){
+    var i=0;
+    
+    $(window).resize(function() {
+        var heights = $(window).height();
+        var widths = $(window).width();
+    for(var up of $ups){
+        if(widths<=991){
+            $footer_option.css("display","none");
             up.onclick=function(){
-                if($html.scrollWidth<=991){
                 this.nextElementSibling.style.display="block";
                 }
+            }else if(widths>991){
+                $footer_option.css("display","block");
             }
-        }
-        for(var dow of footerCosle){
-            dow.onclick=function(){
-                if($html.scrollWidth<=991){
+    }
+
+    for(var dow of footerCosle){
+        if(widths<=991){
+        dow.onclick=function(){
                 this.parentElement.style.display="none";
             }
         }
     }
+})       
+        
 
 })();
 
